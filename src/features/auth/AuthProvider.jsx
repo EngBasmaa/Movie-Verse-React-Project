0f71a2784e7d26646de60b84f3d9e1a2436bb0e6
EngBasmaa <basmaelmasry263@gmail.com>
2025-05-25 09:56:10 +0300

    files updates

--- src/features/auth/AuthProvider.jsx
+++ src/features/auth/AuthProvider.jsx
@@ -1,6 +1,6 @@
 // src/features/auth/context/AuthProvider.jsx
 import { useState, useEffect } from "react";
-import { AuthContext } from "./AuthContext";
+import { AuthContext } from "./authContext";
 import Cookies from "js-cookie";
 
 export const AuthProvider = ({ children }) => {
@@ -41,7 +41,7 @@ export const AuthProvider = ({ children }) => {
         login,
         logout,
         isHeader,
-        setIsHeader,
+        setIsHeader
       }}
     >
       {children}
